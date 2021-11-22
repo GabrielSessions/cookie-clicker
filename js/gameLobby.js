758a331cd431d204005c622cbd2083b2af8bd467
Gabriel Sessions <57270441+GabrielSessions@users.noreply.github.com>
2021-11-22 12:51:29 -0500

    Update gameLobby.js

--- js/gameLobby.js
+++ js/gameLobby.js
@@ -1,4 +1,7 @@
-
+//gameLobby.js
+//By: Gabriel Sessions
+//Purpose: Sets up an online lobby for players to join a game
+//Note: There are referneces to other files in this project (JS functions, CSS, and the main HTML page)
 
 //GAME STATUS NUMBERS
 //0 = Lobby is open
@@ -7,6 +10,8 @@
 var joinedLobby = false;
 var joinedGame = false;
 var lobbyCountInterval;
+var checkGM;
+var isGamemaster = false;
 
 //Assign Random Number 0 to 999 as player ID
 var playerNumber = Math.floor(Math.random() * 1000);
@@ -93,6 +98,10 @@ function joinGame(){
 
 }
 
+
+
+
+
 //Either the user to join or leave game
 function joinLeaveGame(){
     
@@ -121,6 +130,18 @@ function joinLeaveGame(){
 
             }
 
+            //remove gamemaster controls if gamemaster
+            if (isGamemaster){
+                isGamemaster = false;
+                myAirtable.setEntryValueStrict('Gamemaster', 'null');
+
+            }
+
+            else{
+                document.getElementById('gamemasterDisplay').remove();
+            }
+            
+
         }
 
         //If not in game, button will allow user to join the game
@@ -128,8 +149,35 @@ function joinLeaveGame(){
             joinedGame = true;
 
             //Make the first player to join a game the gamemaster
-            if (myAirtable.getEntryValue('NumPlayers') == 0){
+            //If there is no gamemaster, next to join becomes gamemaster
+            if (myAirtable.getEntryValue('NumPlayers') == 0 || myAirtable.getEntryValue('Gamemaster') == ''){
                 gameMasterControls();
+            }
+
+            //Displays who is gamemaster
+            else{
+                console.log("running!");
+                var divGamemasterDisplay = document.createElement('div');
+                divGamemasterDisplay.setAttribute('class', 'logon');
+                divGamemasterDisplay.setAttribute('id', 'divGM');
+
+                var gamemasterDisplay = document.createElement('h3');
+                gamemasterDisplay.setAttribute('id', 'gamemasterDisplay')
+                gamemasterDisplay.innerHTML = "Waiting for " + myAirtable.getEntryValue('Gamemaster') + " to start the game";
+
+                divGamemasterDisplay.append(gamemasterDisplay);
+                document.body.appendChild(divGamemasterDisplay);
+
+                //While in lobby check who is gamemaster
+                
+                checkGM = setInterval(() => {
+                    var displayText = "Waiting for " + myAirtable.getEntryValue('Gamemaster') + " to start the game";
+                    var curText = document.getElementById('gamemasterDisplay').innerHTML;
+                    if (displayText != curText){
+                        curText = displayText;
+                    }
+                    
+                }, 100); 
             }
 
             document.getElementById('joinGameButton').innerHTML = "Leave Game";
@@ -147,6 +195,10 @@ function joinLeaveGame(){
 
 //The gamemaster has the ability to start a game
 function gameMasterControls(){
+    isGamemaster = true;
+
+    //sets as gamemaster in database
+    myAirtable.setEntryValueNotStrict('Gamemaster', username);
 
     var startButtonDiv = document.createElement('div');
     startButtonDiv.setAttribute('class', 'logon');
@@ -170,6 +222,9 @@ function gameMasterControls(){
 
 function startGame(){
     myAirtable.setEntryValueStrict('GameStatus', 1);
+    if (!isGamemaster){
+        document.getElementById('gamemasterDisplay').remove();
+    }
 }
 
 
@@ -178,6 +233,7 @@ function checkIfGameStarted(){
         if (myAirtable.getEntryValue('GameStatus') == 1){
             clearInterval(lobbyCountInterval);
             clearInterval(gameStartCheck);
+            clearInterval(checkGM);
             try{
                 document.getElementById('startGameButton').remove();
             }
